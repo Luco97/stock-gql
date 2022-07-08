fefb551fa055e6cc796b2da3a97538cf829048fc
Luco97 <renatoluco@gmail.com>
2022-07-08 18:52:00 -0400

    UPDATE class-validators para clase que crea items

--- src/database/item/inputs/create.input.ts
+++ src/database/item/inputs/create.input.ts
@@ -1,14 +1,18 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsDefined, Min, MinLength } from 'class-validator';
 import { ItemEntity } from '../model/item-entity';
 
 @InputType()
 export class CreateInput implements Partial<ItemEntity> {
   @Field(() => String)
+  @IsDefined()
+  @MinLength(5)
   name: string;
 
   @Field(() => String)
   imageUrl: string;
 
   @Field(() => Number)
+  @Min(0)
   stock: number;
 }
