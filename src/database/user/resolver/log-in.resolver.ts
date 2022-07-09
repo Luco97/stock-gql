45f998edc068a77e78860576dd21e65488afc14a
Luco97 <renatoluco@gmail.com>
2022-07-09 19:55:38 -0400

    ADD resolver para login mediante resolver

--- src/database/user/resolver/log-in.resolver.ts
+++ src/database/user/resolver/log-in.resolver.ts
@@ -1,12 +1,53 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { HttpStatus } from '@nestjs/common';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+
+import { compare } from 'bcrypt';
+
+import { AuthService } from '@Shared/auth';
+import { LogInInput } from '../inputs/log-in.input';
+import { LogInOutput } from '../outputs/log-in.output';
 import { UserRepositoryService } from '../repository/user-repository.service';
-import { response } from '../outputs/response.output';
-import { LogIn } from '../inputs/log-in.input';
 
 @Resolver()
 export class LogInResolver {
-  constructor(private _userRepo: UserRepositoryService) {}
+  constructor(
+    private _authService: AuthService,
+    private _userRepo: UserRepositoryService,
+  ) {}
 
-  @Mutation(() => response)
-  async logIn(@Args('user') loginUser: LogIn) {}
+  @Mutation(() => LogInOutput)
+  async logIn(
+    @Args('user') loginUser: LogInInput,
+    @Context() context,
+  ): Promise<LogInOutput> {
+    console.log(context);
+    const { email, password } = loginUser;
+    const findOne = await this._userRepo.userRepo
+      .createQueryBuilder('user')
+      .select(['user.id', 'user.username', 'user.email', 'user.password'])
+      .where('user.email = :email', { email })
+      .getOne();
+
+    if (!findOne)
+      return {
+        status: HttpStatus.NOT_FOUND,
+        message: `user doesn't exist`,
+        token: '',
+      };
+    const isValid = await compare(password, findOne.password);
+    if (!isValid)
+      return {
+        status: HttpStatus.NOT_FOUND,
+        message: `user doesn't exist`,
+        token: '',
+      };
+    return {
+      status: HttpStatus.OK,
+      message: 'user logged',
+      token: this._authService.genJWT({
+        id: findOne.id,
+        name: findOne.username,
+      }),
+    };
+  }
 }
