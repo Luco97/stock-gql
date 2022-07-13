fcfbcf48671da73e1793cbc7ccafb8a080f1a310
Luco97 <renatoluco@gmail.com>
2022-07-13 00:20:07 -0400

    UPDATE se realiza fix para enviar of(false) y se agrega inyeccion para uso de setMetaData por rol

--- src/database/guards/role.guard.ts
+++ src/database/guards/role.guard.ts
@@ -1,12 +1,16 @@
+import { Reflector } from '@nestjs/core';
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { AuthService } from '@Shared/auth';
+
 import { Request } from 'express';
 import { from, map, Observable, of } from 'rxjs';
+
+import { AuthService } from '@Shared/auth';
 import { UserRepositoryService } from '../user/repository/user-repository.service';
 
 @Injectable()
 export class RoleGuard implements CanActivate {
   constructor(
+    private _reflector: Reflector,
     private _authService: AuthService,
     private _userService: UserRepositoryService,
   ) {}
@@ -14,14 +18,16 @@ export class RoleGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const req: Request = context.switchToHttp().getNext().req;
-    if (this._authService.validateToken(req.headers?.authorization))
+    const role = this._reflector.get<string>('role', context.getHandler());
+    if (!this._authService.validateToken(req.headers?.authorization))
       return of(false);
     const id_user = this._authService.userID(req.headers?.authorization);
     return from(
       this._userService.userRepo
         .createQueryBuilder('user')
-        .select(['user.type'])
+        .select(['user.id', 'user.type'])
         .where('user.id = :id_user', { id_user })
+        .andWhere('user.type = :role', { role })
         .getCount(),
     ).pipe(map<number, boolean>((count) => (count ? true : false)));
   }
