f2643140a3c13fff4ad5d421fdf1cdcc57b9fa88
Luco97 <renatoluco@gmail.com>
2022-07-13 00:25:07 -0400

    ADD resolver para elimitar productos (solo admin es capaz de borrar productos)

---
+++ src/database/item/resolver/delete-item.resolver.ts
@@ -0,0 +1,26 @@
+import { SetMetadata, UseGuards } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+
+import { RoleGuard } from '../../guards/role.guard';
+import { ChangeOutput } from '../outputs/change.output';
+import { ItemRepositoryService } from '../repository/item-repository.service';
+
+@Resolver()
+export class DeleteItemResolver {
+  constructor(private _itemService: ItemRepositoryService) {}
+
+  @Mutation(() => ChangeOutput)
+  @SetMetadata('role', 'admin')
+  @UseGuards(RoleGuard)
+  async delete(
+    @Args('id_item', { name: 'id_item' }) id: number,
+  ): Promise<ChangeOutput> {
+    const item = await this._itemService.itemRepo
+      .createQueryBuilder('item')
+      .where('item.id = :id', { id })
+      .getOne();
+    if (!item) return { message: `item with id = ${id} doesn't exist` };
+    await this._itemService.itemRepo.softDelete(id);
+    return { message: 'soft deleted item', item };
+  }
+}
