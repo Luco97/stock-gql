cbdc173e5a78be9dacb8d2dee3e2e73f812c8db1
Luco97 <renatoluco@gmail.com>
2022-07-13 00:36:05 -0400

    UPDATE AppModule se queda de esta forma y punto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
+import { AppController } from './app.controller';
+
+import { AuthModule } from './shared/auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DatabaseModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
