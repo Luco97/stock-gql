ff9155704fa15a89523a469d2b5bd2f72c038b3f
Luco97 <renatoluco@gmail.com>
2022-07-15 19:53:27 -0400

    UPDATE valor cambia a 10 items

--- src/database/item/inputs/read.input.ts
+++ src/database/item/inputs/read.input.ts
@@ -2,7 +2,7 @@ import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class ReadInput {
-  @Field(() => Number, { nullable: true, defaultValue: 3 })
+  @Field(() => Number, { nullable: true, defaultValue: 10 })
   take: number;
 
   @Field(() => Number, { nullable: true, defaultValue: 0 })
