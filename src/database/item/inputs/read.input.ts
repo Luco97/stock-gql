321bb4c250759157eed3329bbc7c7de28c33440f
Luco97 <renatoluco@gmail.com>
2022-07-15 00:28:52 -0400

    ADD orderBy y order para input de findAll

--- src/database/item/inputs/read.input.ts
+++ src/database/item/inputs/read.input.ts
@@ -2,9 +2,15 @@ import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 export class ReadInput {
-  @Field(() => Number, {nullable: true, defaultValue: 3})
+  @Field(() => Number, { nullable: true, defaultValue: 3 })
   take: number;
 
   @Field(() => Number, { nullable: true, defaultValue: 0 })
-  skip  : number;
+  skip: number;
+
+  @Field(() => String, { nullable: true, defaultValue: 'createdAt' })
+  orderBy: string;
+
+  @Field(() => String, { nullable: true, defaultValue: 'ASC' })
+  order: 'ASC' | 'DESC';
 }
