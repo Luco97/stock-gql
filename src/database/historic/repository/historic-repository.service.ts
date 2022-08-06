87a736c54592fcd50bad1b3b0943baa0202c2399
Luco97 <renatoluco@gmail.com>
2022-08-05 21:05:36 -0400

    ADD injeccion de repositorio de historic

--- src/database/historic/repository/historic-repository.service.ts
+++ src/database/historic/repository/historic-repository.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { HistoricEntity } from '../model/historic-entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class HistoricRepositoryService {}
+export class HistoricRepositoryService {
+  constructor(
+    @InjectRepository(HistoricEntity)
+    private _changeRepo: Repository<HistoricEntity>,
+  ) {}
+
+  get changeRepo(): Repository<HistoricEntity> {
+    return this._changeRepo;
+  }
+}
