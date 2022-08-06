856b78476a0dbf65052c8c28614c55883e5f07c2
Luco97 <renatoluco@gmail.com>
2022-08-05 21:06:12 -0400

    ADD resolver para obtener cambios (sea admin o usuario, logica similar en ambos)

--- src/database/historic/resolver/read-historic.resolver.ts
+++ src/database/historic/resolver/read-historic.resolver.ts
@@ -1,4 +1,68 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Context, Query, Resolver } from '@nestjs/graphql';
+import { SetMetadata, UseGuards } from '@nestjs/common';
+
+import { Request } from 'express';
+import { Brackets } from 'typeorm';
+
+import { AuthService } from '@Shared/auth';
+import { ReadInput } from '../inputs/read.input';
+import { RoleGuard } from '../../guards/role.guard';
+import { HistoricOutput } from '../outputs/historic.output';
+import { HistoricRepositoryService } from '../repository/historic-repository.service';
 
 @Resolver()
-export class ReadHistoricResolver {}
+export class ReadHistoricResolver {
+  constructor(
+    private _authService: AuthService,
+    private _historicRepo: HistoricRepositoryService,
+  ) {}
+
+  @Query(() => HistoricOutput)
+  @SetMetadata('roles', ['basic', 'admin'])
+  @UseGuards(RoleGuard)
+  async changes(@Args('itemId') getInput: ReadInput, @Context() context) {
+    const req: Request = context.req;
+    const token: string = req.headers?.authorization;
+    const type: string = this._authService.userType(token);
+
+    const { itemId, order } = getInput;
+
+    if (type == 'basic') {
+      const [changes, count] = await this._historicRepo.changeRepo
+        .createQueryBuilder('changes')
+        .leftJoin('changes.item', 'item')
+        .leftJoin('item.user', 'user')
+        .where('item.id = :itemId', { itemId })
+        .andWhere('user.id = :id_user', {
+          id_user: this._authService.userID(token),
+        })
+        .orderBy(
+          'changes.createdAt',
+          ['ASC', 'DESC'].includes(order) ? order : 'ASC',
+        )
+        .getManyAndCount();
+      return { changes, count };
+    } else {
+      const [changes, count] = await this._historicRepo.changeRepo
+        .createQueryBuilder('changes')
+        .leftJoin('changes.item', 'item')
+        .leftJoin('item.user', 'user')
+        .where('item.id = :itemId', { itemId })
+        .andWhere(
+            new Brackets((qb) =>
+              qb
+                .where('user.type = :type', { type: 'basic' })  // changes from normal user
+                .orWhere('user.id = :id_user', {                // OR changes from actual admin user
+                  id_user: this._authService.userID(token),
+                }),
+            ),
+          )
+        .orderBy(
+          'changes.createdAt',
+          ['ASC', 'DESC'].includes(order) ? order : 'ASC',
+        )
+        .getManyAndCount();
+      return { changes, count };
+    }
+  }
+}
