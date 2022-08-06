bdaf36420550c21d37411d6c6152dec69d1281b0
Luco97 <renatoluco@gmail.com>
2022-08-05 21:05:01 -0400

    ADD entidad de historico con sus columnas (bastante basico y probando aspectos relacionados a gql)

--- src/database/historic/model/historic-entity.ts
+++ src/database/historic/model/historic-entity.ts
@@ -1,1 +1,31 @@
-export class HistoricEntity {}
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { ItemEntity } from '../../item/model/item-entity';
+
+@Entity()
+@ObjectType()
+export class HistoricEntity {
+  @Field(() => ID)
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field(() => String)
+  @Column({ type: 'varchar' })
+  change: string;
+
+  @Field(() => Date)
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt: Date;
+
+//   @Field(() => ItemEntity, { nullable: true })
+  @ManyToOne(() => ItemEntity, (item) => item.changes)
+  item: ItemEntity;
+}
