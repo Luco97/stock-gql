77d1682b39b4ae133b98cd6fdc73bda19d722462
Luco97 <renatoluco@gmail.com>
2022-08-08 20:38:34 -0400

    UPDATE se da descripcion a campos de input para obtener items

--- src/database/item/inputs/read.input.ts
+++ src/database/item/inputs/read.input.ts
@@ -1,17 +1,33 @@
-import { ArgsType, Field, InputType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 
 @InputType()
 // @ArgsType() // No funciona como dice la documentacion, seguir usando InputType
 export class ReadInput {
-  @Field(() => Number, { nullable: true, defaultValue: 10 })
+  @Field(() => Number, {
+    nullable: true,
+    defaultValue: 10,
+    description: 'quantity of items',
+  })
   take: number;
 
-  @Field(() => Number, { nullable: true, defaultValue: 0 })
+  @Field(() => Number, {
+    nullable: true,
+    defaultValue: 0,
+    description: 'number of items to skip',
+  })
   skip: number;
 
-  @Field(() => String, { nullable: true, defaultValue: 'createdAt' })
+  @Field(() => String, {
+    nullable: true,
+    defaultValue: 'createdAt',
+    description: 'field selected for order',
+  })
   orderBy: string;
 
-  @Field(() => String, { nullable: true, defaultValue: 'ASC' })
+  @Field(() => String, {
+    nullable: true,
+    defaultValue: 'ASC',
+    description: 'if is ASCendent or DESCendent',
+  })
   order: 'ASC' | 'DESC';
 }
