edac2e076ced9e78b8df5a4ec0b2e7f4117798c6
Luco97 <renatoluco@gmail.com>
2022-08-09 21:04:25 -0400

    UPDATE se crean distintos updates (uno por campo para historico de cambios)

--- src/database/item/inputs/update.input.ts
+++ src/database/item/inputs/update.input.ts
@@ -2,23 +2,46 @@ import { Field, InputType } from '@nestjs/graphql';
 
 import { IsOptional, Min, MinLength } from 'class-validator';
 
-import { ItemEntity } from '../model/item-entity';
+// @InputType()
+// export class UpdateInput implements Partial<ItemEntity> {
+//   @Field(() => Number)
+//   @Min(1)
+//   id_item: number;
 
+//   @Field(() => String, { nullable: true })
+//   @IsOptional()
+//   @MinLength(5)
+//   name: string;
+
+//   @Field(() => String, { nullable: true })
+//   @IsOptional()
+//   imageUrl: string;
+
+//   @Field(() => Number, { nullable: true })
+//   @IsOptional()
+//   @Min(0)
+//   stock: number;
+// }
 @InputType()
-export class UpdateInput implements Partial<ItemEntity> {
+export class UpdateInput {
   @Field(() => Number)
   @Min(1)
   id_item: number;
-
+}
+export class UpdateNameInput extends UpdateInput {
   @Field(() => String, { nullable: true })
   @IsOptional()
   @MinLength(5)
   name: string;
-
+}
+@InputType()
+export class UpdateImageInput extends UpdateInput {
   @Field(() => String, { nullable: true })
   @IsOptional()
   imageUrl: string;
-
+}
+@InputType()
+export class UpdateStockInput extends UpdateInput {
   @Field(() => Number, { nullable: true })
   @IsOptional()
   @Min(0)
