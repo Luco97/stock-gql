c36be033c102698cfd05c6a63ffb7e3b17669cbf
Luco97 <renatoluco@gmail.com>
2022-08-10 21:59:09 -0400

    UPDATE ahora el update de items no es asincrono (se evita uso de awaits en este proceso)

--- src/database/item/resolver/update-item.resolver.ts
+++ src/database/item/resolver/update-item.resolver.ts
@@ -3,19 +3,26 @@ import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 import { Request } from 'express';
 import { Brackets } from 'typeorm';
 
+import {
+  UpdateInput,
+  UpdateNameInput,
+  UpdateStockInput,
+  UpdateImageInput,
+} from '../inputs/update.input';
 import { AuthService } from '@Shared/auth';
 import { ItemEntity } from '../model/item-entity';
 import { RoleGuard } from '../../guards/role.guard';
-import { UpdateInput } from '../inputs/update.input';
 import { ChangeOutput } from '../outputs/change.output';
 import { SetMetadata, UseGuards } from '@nestjs/common';
 import { ItemRepositoryService } from '../repository/item-repository.service';
+import { HistoricRepositoryService } from '../../historic/repository/historic-repository.service';
 
 @Resolver()
 export class UpdateItemResolver {
   constructor(
     private _authService: AuthService,
     private _itemService: ItemRepositoryService,
+    private _historicService: HistoricRepositoryService,
   ) {}
 
   async getItem(itemUpdate: UpdateInput, context): Promise<ItemEntity> {
@@ -23,9 +30,9 @@ export class UpdateItemResolver {
     const token: string = req.headers?.authorization;
     const type: string = this._authService.userType(token);
     const { id_item } = itemUpdate;
-    let item: ItemEntity;
+    let item: Promise<ItemEntity>;
     if (type == 'basic')
-      item = await this._itemService.itemRepo
+      item = this._itemService.itemRepo
         .createQueryBuilder('item')
         .leftJoin('item.user', 'user')
         .where('item.id = :id_item', { id_item })
@@ -34,7 +41,7 @@ export class UpdateItemResolver {
         })
         .getOne();
     else
-      item = await this._itemService.itemRepo
+      item = this._itemService.itemRepo
         .createQueryBuilder('item')
         .leftJoin('item.user', 'user')
         .where(
@@ -61,63 +68,113 @@ export class UpdateItemResolver {
     return item;
   }
 
+  // @Mutation(() => ChangeOutput)
+  // @SetMetadata('roles', ['basic', 'admin'])
+  // @UseGuards(RoleGuard)
+  // async update(
+  //   @Args('item') itemUpdate: UpdateInput,
+  //   @Context() context,
+  // ): Promise<ChangeOutput> {
+  //   const req: Request = context.req;
+  //   const token: string = req.headers?.authorization;
+  //   const type: string = this._authService.userType(token);
+  //   const {
+  //     id_item,
+  //     // , imageUrl, name, stock
+  //   } = itemUpdate;
+  //   let item: ItemEntity;
+  //   if (type == 'basic')
+  //     item = await this._itemService.itemRepo
+  //       .createQueryBuilder('item')
+  //       .leftJoin('item.user', 'user')
+  //       .where('item.id = :id_item', { id_item })
+  //       .andWhere('user.id = :id_user', {
+  //         id_user: this._authService.userID(token),
+  //       })
+  //       .getOne();
+  //   else
+  //     item = await this._itemService.itemRepo
+  //       .createQueryBuilder('item')
+  //       .leftJoin('item.user', 'user')
+  //       .where(
+  //         // Item propio del admin
+  //         new Brackets((qb) =>
+  //           qb
+  //             .where('item.id = :id_item', { id_item })
+  //             .andWhere('user.id = :id_user', {
+  //               id_user: this._authService.userID(token),
+  //             }),
+  //         ),
+  //       )
+  //       .orWhere(
+  //         // Item de un usario basic
+  //         new Brackets((qb) =>
+  //           qb
+  //             .where('item.id = :id_item', { id_item })
+  //             .andWhere('user.type = :type', {
+  //               type: 'basic',
+  //             }),
+  //         ),
+  //       )
+  //       .getOne();
+
+  //   if (!item) return { message: `item with id = ${id_item} doesn't exist` };
+  //   const updateItem = await this._itemService.itemRepo.save({
+  //     id: item.id,
+  //     // name: name || item.name,
+  //     // stock: stock || item.stock,
+  //     // imageUrl: imageUrl || item.imageUrl,
+  //   });
+  //   return { message: 'updated item', item: updateItem };
+  // }
+
   @Mutation(() => ChangeOutput)
   @SetMetadata('roles', ['basic', 'admin'])
   @UseGuards(RoleGuard)
-  async update(
-    @Args('item') itemUpdate: UpdateInput,
+  async updateName(
+    @Args('item') itemNameUpdate: UpdateNameInput,
     @Context() context,
   ): Promise<ChangeOutput> {
-    const req: Request = context.req;
-    const token: string = req.headers?.authorization;
-    const type: string = this._authService.userType(token);
-    const {
-      id_item,
-      // , imageUrl, name, stock
-    } = itemUpdate;
-    let item: ItemEntity;
-    if (type == 'basic')
-      item = await this._itemService.itemRepo
-        .createQueryBuilder('item')
-        .leftJoin('item.user', 'user')
-        .where('item.id = :id_item', { id_item })
-        .andWhere('user.id = :id_user', {
-          id_user: this._authService.userID(token),
-        })
-        .getOne();
-    else
-      item = await this._itemService.itemRepo
-        .createQueryBuilder('item')
-        .leftJoin('item.user', 'user')
-        .where(
-          // Item propio del admin
-          new Brackets((qb) =>
-            qb
-              .where('item.id = :id_item', { id_item })
-              .andWhere('user.id = :id_user', {
-                id_user: this._authService.userID(token),
-              }),
-          ),
-        )
-        .orWhere(
-          // Item de un usario basic
-          new Brackets((qb) =>
-            qb
-              .where('item.id = :id_item', { id_item })
-              .andWhere('user.type = :type', {
-                type: 'basic',
-              }),
-          ),
-        )
-        .getOne();
-
-    if (!item) return { message: `item with id = ${id_item} doesn't exist` };
-    const updateItem = await this._itemService.itemRepo.save({
-      id: item.id,
-      // name: name || item.name,
-      // stock: stock || item.stock,
-      // imageUrl: imageUrl || item.imageUrl,
+    const { id_item, name } = itemNameUpdate;
+    return new Promise<ChangeOutput>((resolve, reject) => {
+      this.getItem({ id_item }, context).then((item) => {
+        if (!item)
+          resolve({ message: `item with id = ${id_item} doesn't exist` });
+        this._itemService.itemRepo
+          .save({
+            id: item.id,
+            name,
+          })
+          .then((updatedItem) => {
+            this._historicService.changeRepo
+              .save(
+                this._historicService.changeRepo.create({
+                  previousValue: `${item.name}`,
+                  change: 'Name',
+                }),
+              )
+              .then((change) => {
+                this._historicService.changeRepo
+                  .createQueryBuilder('change')
+                  .relation('item')
+                  .of(change.id)
+                  .set(item.id)
+                  .finally(() => {
+                    resolve({
+                      message: 'updated item',
+                      item: {
+                        id: item.id,
+                        stock: item.stock,
+                        createdAt: item.createdAt,
+                        imageUrl: item.imageUrl,
+                        name: updatedItem.name, // UPDATE elements
+                        updatedAt: updatedItem.updatedAt, // UPDATE elements
+                      },
+                    });
+                  });
+              });
+          });
+      });
     });
-    return { message: 'updated item', item: updateItem };
   }
 }
