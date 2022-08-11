34afa491df00ba582a0ff39fb1faa0e8e4febd05
Luco97 <renatoluco@gmail.com>
2022-08-10 21:57:56 -0400

    UPDATE entidad en output es deepPartial para evitar que todos los datos sean requeridos al construir objeto

--- src/database/item/outputs/change.output.ts
+++ src/database/item/outputs/change.output.ts
@@ -1,4 +1,5 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { DeepPartial } from 'typeorm';
 
 import { ItemEntity } from '../model/item-entity';
 
@@ -8,5 +9,5 @@ export class ChangeOutput {
   message: string;
 
   @Field(() => ItemEntity, { nullable: true })
-  item?: ItemEntity;
+  item?: DeepPartial<ItemEntity>;
 }
