6f24a8432fe784360d1762ef56c502b70acef83b
Luco97 <renatoluco@gmail.com>
2022-08-11 20:04:10 -0400

    UPDATE se quitan opciones para los campos y se agrega nuevo input para el precio

--- src/database/item/inputs/update.input.ts
+++ src/database/item/inputs/update.input.ts
@@ -22,29 +22,37 @@ import { IsOptional, Min, MinLength } from 'class-validator';
 //   @Min(0)
 //   stock: number;
 // }
+
 @InputType()
 export class UpdateInput {
   @Field(() => Number)
   @Min(1)
   id_item: number;
 }
+
 @InputType()
 export class UpdateNameInput extends UpdateInput {
   @Field(() => String)
-  @IsOptional()
   @MinLength(5)
   name: string;
 }
+
 @InputType()
 export class UpdateImageInput extends UpdateInput {
-  @Field(() => String, { nullable: true })
-  @IsOptional()
+  @Field(() => String)
   imageUrl: string;
 }
+
 @InputType()
 export class UpdateStockInput extends UpdateInput {
-  @Field(() => Number, { nullable: true })
-  @IsOptional()
+  @Field(() => Number)
   @Min(0)
   stock: number;
+}
+
+@InputType()
+export class UpdatePriceInput extends UpdateInput {
+  @Field(() => Number)
+  @Min(0)
+  price: number;
 }
