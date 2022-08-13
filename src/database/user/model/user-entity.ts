4ea354dad4e16956aa11f6a72747193f351324a7
Luco97 <renatoluco@gmail.com>
2022-08-12 22:32:42 -0400

    UPDATE vuelve objeto para GQL dado que admins necesitan saber usuario y pertenece al esquema

--- src/database/user/model/user-entity.ts
+++ src/database/user/model/user-entity.ts
@@ -1,3 +1,5 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+
 import {
   Column,
   Entity,
@@ -10,22 +12,29 @@ import { genSalt, hash } from 'bcrypt';
 import { ItemEntity } from '../../item/model/item-entity';
 
 @Entity()
+@ObjectType()
 export class UserEntity {
+  @Field(() => ID)
   @PrimaryGeneratedColumn('increment')
   id: number;
 
+  @Field(() => String)
   @Column({ type: 'varchar' })
   email: string;
 
+  @Field(() => String)
   @Column({ type: 'varchar' })
   username: string;
 
+  @Field(() => String)
   @Column({ type: 'varchar', select: false })
   password: string;
 
+  @Field(() => String)
   @Column({ type: 'varchar', default: 'basic' })
   type: string;
 
+  @Field(() => [ItemEntity], { nullable: true })
   @OneToMany(() => ItemEntity, (items) => items.user)
   items: ItemEntity[];
 
