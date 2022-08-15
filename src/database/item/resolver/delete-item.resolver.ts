18c61a0dbe34bf9aa5c56a6a36b8de7872c05af8
Luco97 <renatoluco@gmail.com>
2022-08-14 20:06:42 -0400

    UPDATE metodo para eliminar items evitando el uso de awaits en promesas

--- src/database/item/resolver/delete-item.resolver.ts
+++ src/database/item/resolver/delete-item.resolver.ts
@@ -15,12 +15,17 @@ export class DeleteItemResolver {
   async delete(
     @Args('id_item', { name: 'id_item' }) id: number,
   ): Promise<ChangeOutput> {
-    const item = await this._itemService.itemRepo
-      .createQueryBuilder('item')
-      .where('item.id = :id', { id })
-      .getOne();
-    if (!item) return { message: `item with id = ${id} doesn't exist` };
-    await this._itemService.itemRepo.softDelete(id);
-    return { message: 'soft deleted item', item };
+    return new Promise<ChangeOutput>((resolve, reject) => {
+      this._itemService.itemRepo
+        .createQueryBuilder('item')
+        .where('item.id = :id', { id })
+        .getOne()
+        .then((item) => {
+          if (!item) resolve({ message: `item with id = ${id} doesn't exist` });
+          this._itemService.itemRepo
+            .softDelete(id)
+            .then(() => resolve({ message: 'soft deleted item', item }));
+        });
+    });
   }
 }
