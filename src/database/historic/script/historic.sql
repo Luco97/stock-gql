29ac69adcfc50141994270cdc5d28e69384179a4
Luco97 <renatoluco@gmail.com>
2022-08-26 21:28:37 -0400

    ADD scripts de historico de items

---
+++ src/database/historic/script/historic.sql
@@ -0,0 +1,18 @@
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('name', '2022-08-06 00:01:25.393', 27, NULL);
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('name', '2022-08-06 00:11:38.010', 27, NULL);
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('stock', '2022-08-06 00:11:38.011', 27, NULL);
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('name', '2022-08-06 00:01:25.393', 2, NULL);
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('name', '2022-08-06 00:11:38.010', 2, NULL);
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('stock', '2022-08-06 00:11:38.011', 2, NULL);
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('Name', '2022-08-10 21:09:06.789', 8, NULL);
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('Name', '2022-08-10 21:39:49.670', 8, 'Agua con vino');
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('Name', '2022-08-10 21:41:38.949', 8, 'Marcos para el titulo');
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('Name', '2022-08-10 21:41:54.672', 8, 'cabritas');
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('Name', '2022-08-10 21:42:35.316', 8, 'cabritas');
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('Name', '2022-08-10 21:45:32.503', 8, 'Marcos para el titulo');
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('Name', '2022-08-10 21:45:57.276', 8, 'Marcos para el titulo');
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('Name', '2022-08-10 21:50:09.076', 8, 'cabritas');
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('Name', '2022-08-10 21:52:55.908', 8, 'agua con vinito');
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('Name', '2022-08-10 21:54:12.104', 8, 'achelita');
+INSERT INTO historic_entity ("change", "createdAt", "itemId", "previousValue") VALUES('Name', '2022-08-10 21:54:26.600', 8, 'achelita');
+-- Recordad primero crear items
