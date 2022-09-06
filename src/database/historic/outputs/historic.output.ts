2e6fa0605952addf0a54b0a3ea1d6319a2e48216
Luco97 <renatoluco@gmail.com>
2022-09-05 23:11:06 -0300

    UPDATE descripcion en objectType para outputs de cambios

--- src/database/historic/outputs/historic.output.ts
+++ src/database/historic/outputs/historic.output.ts
@@ -4,9 +4,9 @@ import { HistoricEntity } from '../model/historic-entity';
 
 @ObjectType()
 export class HistoricOutput {
-  @Field(() => [HistoricEntity])
+  @Field(() => [HistoricEntity], { description: 'array of changes in item' })
   changes: HistoricEntity[];
 
-  @Field(() => Number)
+  @Field(() => Number, { description: 'quantity of changes in item' })
   count: number;
 }
