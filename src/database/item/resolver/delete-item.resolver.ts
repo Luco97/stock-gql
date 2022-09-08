633d5788aac9949956232f0f1686751cde1326b3
Luco97 <renatoluco@gmail.com>
2022-09-08 20:17:26 -0300

    ADD logica para que admins puedan borrar cualqueir item de usuarios 'basic' o los propios (un admin no puede borrar items de otro admin)

--- src/database/item/resolver/delete-item.resolver.ts
+++ src/database/item/resolver/delete-item.resolver.ts
@@ -1,13 +1,20 @@
 import { SetMetadata, UseGuards } from '@nestjs/common';
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
 
+import { Request } from 'express';
+import { Brackets } from 'typeorm';
+
+import { AuthService } from '@Shared/auth';
 import { RoleGuard } from '../../guards/role.guard';
 import { ChangeOutput } from '../outputs/change.output';
 import { ItemRepositoryService } from '../repository/item-repository.service';
 
 @Resolver()
 export class DeleteItemResolver {
-  constructor(private _itemService: ItemRepositoryService) {}
+  constructor(
+    private _authService: AuthService,
+    private _itemService: ItemRepositoryService,
+  ) {}
 
   @Mutation(() => ChangeOutput, {
     name: 'delete_item',
@@ -17,11 +24,23 @@ export class DeleteItemResolver {
   @UseGuards(RoleGuard)
   async delete(
     @Args('id_item', { name: 'id_item' }) id: number,
+    @Context() context,
   ): Promise<ChangeOutput> {
+    const req: Request = context.req;
+    const token: string = req.headers?.authorization;
+    const adminID: number = this._authService.userID(token);
     return new Promise<ChangeOutput>((resolve, reject) => {
       this._itemService.itemRepo
         .createQueryBuilder('item')
+        .leftJoin('item.user', 'user')
         .where('item.id = :id', { id })
+        .andWhere(
+          new Brackets((qb) =>
+            qb
+              .where('user.type = :type', { type: 'basic' })
+              .orWhere('user.id = :adminID', { adminID }),
+          ),
+        )
         .getOne()
         .then((item) => {
           if (!item) resolve({ message: `item with id = ${id} doesn't exist` });
