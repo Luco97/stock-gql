b2f665fad900fa2704318e789d04f144cd3253b0
Luco97 <renatoluco@gmail.com>
2022-09-13 23:28:51 -0300

    UPDATE descripciones para inputType en clases de update para item

--- src/database/item/inputs/update.input.ts
+++ src/database/item/inputs/update.input.ts
@@ -2,36 +2,41 @@ import { Field, InputType } from '@nestjs/graphql';
 
 import { Min, MinLength } from 'class-validator';
 
-@InputType()
+@InputType({
+  description:
+    'update base class, extends in the others updates mutation input',
+})
 export class UpdateInput {
-  @Field(() => Number)
+  @Field(() => Number, {
+    description: 'item ID',
+  })
   @Min(1)
   id_item: number;
 }
 
-@InputType()
+@InputType({ description: 'update name class' })
 export class UpdateNameInput extends UpdateInput {
-  @Field(() => String)
+  @Field(() => String, { description: 'new name in item' })
   @MinLength(5)
   name: string;
 }
 
-@InputType()
+@InputType({ description: 'update imageURL class' })
 export class UpdateImageInput extends UpdateInput {
-  @Field(() => String)
+  @Field(() => String, { description: 'new imageURL in item' })
   imageUrl: string;
 }
 
-@InputType()
+@InputType({ description: 'update stock class' })
 export class UpdateStockInput extends UpdateInput {
-  @Field(() => Number)
+  @Field(() => Number, { description: 'new stock in item' })
   @Min(0)
   stock: number;
 }
 
-@InputType()
+@InputType({ description: 'update price class' })
 export class UpdatePriceInput extends UpdateInput {
-  @Field(() => Number)
+  @Field(() => Number, { description: 'new price in item' })
   @Min(0)
   price: number;
 }
