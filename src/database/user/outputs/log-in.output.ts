df5d9dff01cc39b4f93a1d2f68a57670b11b027b
Luco97 <renatoluco@gmail.com>
2022-09-14 23:08:44 -0300

    UPDATE descripcion en tipo para output de logIn

--- src/database/user/outputs/log-in.output.ts
+++ src/database/user/outputs/log-in.output.ts
@@ -2,7 +2,7 @@ import { Field, ObjectType } from '@nestjs/graphql';
 
 import { SignInOutput } from './sign-in.output';
 
-@ObjectType()
+@ObjectType({ description: 'output object in log_in mutation' })
 export class LogInOutput extends SignInOutput {
   @Field(() => String, { nullable: true, defaultValue: '' })
   token: string;
