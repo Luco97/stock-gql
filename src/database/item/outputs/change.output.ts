32e92a1b2f9b8192cc4bc15123ab657125c4a235
Luco97 <renatoluco@gmail.com>
2022-09-16 23:51:09 -0300

    UPDATE descripcion a objectType de update

--- src/database/item/outputs/change.output.ts
+++ src/database/item/outputs/change.output.ts
@@ -3,11 +3,16 @@ import { DeepPartial } from 'typeorm';
 
 import { ItemEntity } from '../model/item-entity';
 
-@ObjectType()
+@ObjectType({
+  description: 'response objectType when any items is updated',
+})
 export class ChangeOutput {
-  @Field(() => String, { nullable: true })
+  @Field(() => String, { nullable: true, description: 'message of the update' })
   message: string;
 
-  @Field(() => ItemEntity, { nullable: true })
+  @Field(() => ItemEntity, {
+    nullable: true,
+    description: 'the item with the update fields',
+  })
   item?: DeepPartial<ItemEntity>;
 }
