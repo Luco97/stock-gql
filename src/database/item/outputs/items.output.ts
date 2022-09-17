18f5265ab3856b90ea805f1b5170f9f5d4edf0c4
Luco97 <renatoluco@gmail.com>
2022-09-16 23:49:40 -0300

    UPDATE descripcion a objectType de 'find_all'

--- src/database/item/outputs/items.output.ts
+++ src/database/item/outputs/items.output.ts
@@ -4,9 +4,9 @@ import { ItemEntity } from '../model/item-entity';
 
 @ObjectType({ description: 'response in find_all query' })
 export class ItemsOutput {
-  @Field(() => [ItemEntity])
+  @Field(() => [ItemEntity], { description: 'array with the items' })
   items: ItemEntity[];
 
-  @Field(() => Number)
+  @Field(() => Number, { description: 'number of items in the array' })
   count: number;
 }
