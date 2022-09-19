9b3f3ab2cef0b9764265c0ae05cb04330fe49ec6
Luco97 <renatoluco@gmail.com>
2022-09-19 02:11:38 -0300

    UPDATE nombre para inputs de tipo update

--- src/database/item/inputs/update.input.ts
+++ src/database/item/inputs/update.input.ts
@@ -10,27 +10,27 @@ export class UpdateInput {
   id_item: number;
 }
 
-@InputType({ description: 'update name class' })
+@InputType('update_name', { description: 'update name class' })
 export class UpdateNameInput extends UpdateInput {
   @Field(() => String, { description: 'new name in item' })
   @MinLength(5)
   name: string;
 }
 
-@InputType({ description: 'update imageURL class' })
+@InputType('update_image', { description: 'update imageURL class' })
 export class UpdateImageInput extends UpdateInput {
   @Field(() => String, { description: 'new imageURL in item' })
   imageUrl: string;
 }
 
-@InputType({ description: 'update stock class' })
+@InputType('update_stock', { description: 'update stock class' })
 export class UpdateStockInput extends UpdateInput {
   @Field(() => Number, { description: 'new stock in item' })
   @Min(0)
   stock: number;
 }
 
-@InputType({ description: 'update price class' })
+@InputType('update_price', { description: 'update price class' })
 export class UpdatePriceInput extends UpdateInput {
   @Field(() => Number, { description: 'new price in item' })
   @Min(0)
