e16077cbf42b11143250594ee1512ab29f0219a5
Luco97 <renatoluco@gmail.com>
2022-09-19 02:11:12 -0300

    UPDATE nombre y descripcion para input de login

--- src/database/user/inputs/log-in.input.ts
+++ src/database/user/inputs/log-in.input.ts
@@ -1,7 +1,7 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { IsEmail, MinLength } from 'class-validator';
 
-@InputType()
+@InputType('log_in', { description: 'log in object (with email and password)' })
 export class LogInInput {
   @Field(() => String, { description: 'email of user to log in' })
   @IsEmail()
