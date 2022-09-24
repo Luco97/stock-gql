5366ad86ed578e8a634061ade1cc938adbf5ac14
Luco97 <renatoluco@gmail.com>
2022-09-23 21:26:32 -0300

    UPDATE se crean metodos para acomodar codigo en resolvers

--- src/database/user/repository/user-repository.service.ts
+++ src/database/user/repository/user-repository.service.ts
@@ -14,4 +14,47 @@ export class UserRepositoryService {
   get userRepo(): Repository<UserEntity> {
     return this._userRepo;
   }
+
+  register_user(parameters: {
+    email: string;
+    username: string;
+  }): Promise<number> {
+    const { email, username } = parameters;
+    return this._userRepo
+      .createQueryBuilder('user')
+      .where('user.email = :email', { email })
+      .orWhere('user.username = :username', { username })
+      .getCount();
+  }
+
+  create_user(parameters: {
+    email: string;
+    username: string;
+    password: string;
+  }): Promise<UserEntity> {
+    const { email, password, username } = parameters;
+    return this._userRepo.save(
+      this._userRepo.create({
+        email,
+        password,
+        username,
+        type: 'basic',
+      }),
+    );
+  }
+
+  find_one_by_email(parameters: { email: string }): Promise<UserEntity> {
+    const { email } = parameters;
+    return this._userRepo
+      .createQueryBuilder('user')
+      .select([
+        'user.id',
+        'user.username',
+        'user.email',
+        'user.password',
+        'user.type',
+      ])
+      .where('user.email = :email', { email })
+      .getOne();
+  }
 }
