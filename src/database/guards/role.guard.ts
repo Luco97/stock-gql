1d8464de5ba7585e001b6e39c463f743d5d7e510
Luco97 <renatoluco@gmail.com>
2022-09-24 23:43:09 -0300

    UPDATE se utiliza metodo creado en servicio de usuario para acomodar codigo

--- src/database/guards/role.guard.ts
+++ src/database/guards/role.guard.ts
@@ -18,17 +18,15 @@ export class RoleGuard implements CanActivate {
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const req: Request = context.switchToHttp().getNext().req;
-    const roles = this._reflector.get<string>('roles', context.getHandler());
+    const roles: string[] = this._reflector.get<string[]>(
+      'roles',
+      context.getHandler(),
+    );
     if (!this._authService.validateToken(req.headers?.authorization))
       return of(false);
     const id_user = this._authService.userID(req.headers?.authorization);
     return from(
-      this._userService.userRepo
-        .createQueryBuilder('user')
-        .select(['user.id', 'user.type'])
-        .where('user.id = :id_user', { id_user })
-        .andWhere('user.type IN (:...roles)', { roles })
-        .getCount(),
+      this._userService.find_and_count_roles({ id_user, roles }),
     ).pipe(map<number, boolean>((count) => (count ? true : false)));
   }
 }
