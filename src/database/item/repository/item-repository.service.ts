2365c1daff421330266656da0280b4931e2f820d
Luco97 <renatoluco@gmail.com>
2022-09-26 23:49:18 -0300

    UPDATE algunos metodos con condiciones para tipos de usuario y consultar un item

--- src/database/item/repository/item-repository.service.ts
+++ src/database/item/repository/item-repository.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 
-import { Repository } from 'typeorm';
+import { Repository, Brackets } from 'typeorm';
 
 import { ItemEntity } from '../model/item-entity';
 
@@ -13,5 +13,36 @@ export class ItemRepositoryService {
 
   get itemRepo(): Repository<ItemEntity> {
     return this._itemRepo;
+  }
+
+  basic_condition(id_user: number): Brackets {
+    return new Brackets((qb) =>
+      qb.where('user.id = :id_user', {
+        id_user, // OR changes from actual user
+      }),
+    );
+  }
+
+  admin_condition(id_user: number): Brackets {
+    return new Brackets((qb) =>
+      qb
+        .where('user.type = :type', { type: 'basic' }) // changes from normal user
+        .orWhere('user.id = :id_user', {
+          id_user, // OR changes from actual admin user
+        }),
+    );
+  }
+
+  find_one_item(parameters: {
+    id_item: number;
+    id_user: Brackets;
+  }): Promise<ItemEntity> {
+    const { id_user, id_item } = parameters;
+    return this._itemRepo
+      .createQueryBuilder('item')
+      .leftJoin('item.user', 'user')
+      .where('item.id = :id_item', { id_item })
+      .andWhere(id_user)
+      .getOne();
   }
 }
