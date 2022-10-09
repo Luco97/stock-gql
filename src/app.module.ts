af7c5bbd74f6a5dbaa76b906b69c8d94f5bde103
Luco97 <renatoluco@gmail.com>
2022-10-09 19:36:49 -0300

    UPDATE se quitan controlador y servicio sin uso

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppService } from './app.service';
-import { AppController } from './app.controller';
-
 import { AuthModule } from './shared/auth/auth.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
   imports: [ConfigModule.forRoot(), DatabaseModule, AuthModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
