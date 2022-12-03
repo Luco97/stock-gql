bc0a0cbd78b0e206ac9a5db740ca903408553031
Luco97 <renatoluco@gmail.com>
2022-12-03 00:32:49 -0300

    ADD input para update general

--- src/database/item/inputs/update.input.ts
+++ src/database/item/inputs/update.input.ts
@@ -1,6 +1,6 @@
 import { Field, InputType } from '@nestjs/graphql';
 
-import { Min, MinLength } from 'class-validator';
+import { IsOptional, IsUrl, Min, MinLength } from 'class-validator';
 
 export class UpdateInput {
   @Field(() => Number, {
@@ -35,4 +35,33 @@ export class UpdatePriceInput extends UpdateInput {
   @Field(() => Number, { description: 'new price in item' })
   @Min(0)
   price: number;
+}
+
+@InputType('update_item')
+export class Update {
+  @Field(() => Number, {
+    description: 'item ID',
+  })
+  @Min(1)
+  id_item: number;
+
+  @Field(() => Number, { description: 'new price in item', nullable: true })
+  @IsOptional()
+  @Min(0)
+  price: number;
+
+  @Field(() => Number, { description: 'new stock in item', nullable: true })
+  @IsOptional()
+  @Min(0)
+  stock: number;
+
+  @Field(() => String, { description: 'new imageURL in item', nullable: true })
+  @IsOptional()
+  @IsUrl()
+  imageUrl: string;
+
+  @Field(() => String, { description: 'new name in item', nullable: true })
+  @IsOptional()
+  @MinLength(5)
+  name: string;
 }
