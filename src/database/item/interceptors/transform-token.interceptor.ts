06600191b25bce93c5dcdc8ffc3afbf87dd727d8
Luco97 <renatoluco@gmail.com>
2023-01-05 20:19:21 -0300

    UPDATE se quita codigo comentado

--- src/database/item/interceptors/transform-token.interceptor.ts
+++ src/database/item/interceptors/transform-token.interceptor.ts
@@ -13,22 +13,14 @@ export class TransformTokenInterceptor implements NestInterceptor {
   constructor(private _authService: AuthService) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    console.log('---> ', context);
     const req: Request = context.getArgByIndex(2).req;
     const { id, name, type } = this._authService.getContext(
       req.headers?.authorization,
     );
-    // context.getArgByIndex(2).req.res.set('user_id', id.toString());
-    // context.getArgByIndex(2).req.res.set('user_name', name);
-    // context.getArgByIndex(2).req.res.set('user_type', type);
     req.headers['user_id'] = id.toString();
     req.headers['user_name'] = name;
     req.headers['user_type'] = type;
 
-    // req.res.setHeader(
-    //   'aaa',
-    //   this._authService.userType(req.headers?.authorization),
-    // );
     return next.handle();
   }
 }
