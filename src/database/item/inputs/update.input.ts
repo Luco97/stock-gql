02a8def5df2899b4e516a5762d20e4c65797e5a0
Luco97 <renatoluco@gmail.com>
2023-01-23 18:41:16 -0300

    UPDATE se quitan clases para actualizar un solo campo de item

--- src/database/item/inputs/update.input.ts
+++ src/database/item/inputs/update.input.ts
@@ -10,33 +10,6 @@ export class UpdateInput {
   id_item: number;
 }
 
-@InputType('update_name', { description: 'update name class' })
-export class UpdateNameInput extends UpdateInput {
-  @Field(() => String, { description: 'new name in item' })
-  @MinLength(5)
-  name: string;
-}
-
-@InputType('update_image', { description: 'update imageURL class' })
-export class UpdateImageInput extends UpdateInput {
-  @Field(() => String, { description: 'new imageURL in item' })
-  imageUrl: string;
-}
-
-@InputType('update_stock', { description: 'update stock class' })
-export class UpdateStockInput extends UpdateInput {
-  @Field(() => Number, { description: 'new stock in item' })
-  @Min(0)
-  stock: number;
-}
-
-@InputType('update_price', { description: 'update price class' })
-export class UpdatePriceInput extends UpdateInput {
-  @Field(() => Number, { description: 'new price in item' })
-  @Min(0)
-  price: number;
-}
-
 @InputType('update_item')
 export class Update {
   @Field(() => Number, {
