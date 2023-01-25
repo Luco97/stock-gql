79f44b3b0dd77ac63d1a4199f3b1330078f32844
Luco97 <renatoluco@gmail.com>
2023-01-24 23:16:06 -0300

    CREATE repositorio para operaciones basicas con tags

---
+++ src/database/tag/repository/tag-repository.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository, UpdateResult } from 'typeorm';
+import { TagEntity } from '../model/tag-entity';
+
+@Injectable()
+export class TagRepositoryService {
+  constructor(
+    @InjectRepository(TagEntity) private _tagRepo: Repository<TagEntity>,
+  ) {}
+
+  find_all(params: {
+    name: string;
+    take: number;
+    skip: number;
+  }): Promise<[TagEntity[], number]> {
+    const { skip, take, name } = params;
+    return this._tagRepo
+      .createQueryBuilder('tag')
+      .leftJoin('tag.items', 'items')
+      .loadRelationCountAndMap(
+        'items.item_count',
+        'items.tags',
+        'chips',
+        (qb) => qb.orderBy('cnt'),
+      )
+      .where('LOWER(tag.name) like LOWER(:name)', { name: `${name}%` })
+      .orderBy('tag.name')
+      .take(take || 10)
+      .skip(skip || 0)
+      .getManyAndCount();
+  }
+
+  find_one(ids?: number[], name?: string): Promise<TagEntity[]> {
+    return this._tagRepo
+      .createQueryBuilder('tag')
+      .leftJoin('tag.items', 'items')
+      .loadRelationCountAndMap(
+        'items.item_count',
+        'items.tags',
+        'chips',
+        (qb) => qb.orderBy('cnt'),
+      )
+      .where('LOWER(tag.name) = LOWER(:name)', { name })
+      .orWhere('tag.id in (:...id)', { id: ids })
+      .getMany();
+  }
+
+  //   Admins only
+  create(name: string, description: string): Promise<TagEntity> {
+    return this._tagRepo.save(
+      this._tagRepo.create({ name: name.toLowerCase(), description }),
+    );
+  }
+
+  //   Admins only
+  update(id: number, description: string): Promise<UpdateResult> {
+    return this._tagRepo.update({ id }, { description });
+  }
+}
